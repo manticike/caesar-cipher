8e5d1e21eed25895c69b11537cd666b6025ddf7a
manticike <antwikisaac@gmail.com>
2024-03-11 04:24:33 +0000

    Created the Caesar's cipher
    In the project, I was asked to convert everything
    alphaberts but using the ASCII version I couldn't
    do that
    
    So you'll find all sorts of symbols in the encrypted version

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -5,13 +5,24 @@ puts "Enter your message for encryption: "
 # Get user input and store it in a variable
 message = gets.chomp
 
-# Convert the message to lowercase
-lowercase_message = message.downcase
+# Get the key input from the user
+puts "Please, enter the key for encryption"
+key = gets.chomp
 
-# Ask the user to input the key for the encryption
-puts "What's the key for the encrypton? "
-encryption_key = gets.chomp
+# Convert the key from a string to an integer
+integer_key = key.to_i
+# Convert message to ascii 
+# the ord will turn every element in the message to its ASCII value ie. numbers.
 
+message_to_ascii = message.chars.map { |c| c.ord }
 
-# Display the user input
-puts "Your message is:\n #{lowercase_message}"
+# Now let's shift the message with the key the user will input
+# To reverse the message you'd need to use negative instead of positive
+encrypt_with_key = message_to_ascii.map { |c| c + integer_key}
+
+# Now the encryption
+
+encrypted_message = encrypt_with_key.map { |c| c.chr}.join
+
+# Print the encrypted message
+puts encrypted_message
